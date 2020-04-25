359d89ddab3faa791f3bc76fe091f5e5c036f364
Heera <heera1191@gmail.com>
2020-04-25 10:00:57 +0530

    models added

--- ipl_project/settings.py
+++ ipl_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'matches',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'ipl_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'ipl_project',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Matches(models.Model):
+    season = models.CharField(max_length=6)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    date = models.DateField()
+    team1 = models.CharField(max_length=50)
+    team2 = models.CharField(max_length=50)
+    toss_winner = models.CharField(max_length=50)
+    toss_decision = models.CharField(max_length=20)
+    result = models.CharField(max_length=20)
+    dl_applied = models.IntegerField()
+    winner = models.CharField(max_length=50, null=True, blank=True)
+    win_by_runs = models.IntegerField()
+    win_by_wickets = models.IntegerField()
+    player_of_match = models.CharField(max_length=30, null=True, blank=True)
+    venue = models.CharField(max_length=100)
+    umpire1 = models.CharField(max_length=50, null=True, blank=True)
+    umpire2 = models.CharField(max_length=50, null=True, blank=True)
+    umpire3 = models.CharField(max_length=50, null=True, blank=True)
+
+
+class Deliveries(models.Model):
+    match_id = models.ForeignKey(Matches, on_delete=models.CASCADE, related_name='delivery_matches')
+    inning = models.IntegerField()
+    batting_team = models.CharField(max_length=50)
+    bowling_team = models.CharField(max_length=50)
+    over = models.IntegerField()
+    ball = models.IntegerField()
+    batsman = models.CharField(max_length=50)
+    non_striker = models.CharField(max_length=50)
+    bowler = models.CharField(max_length=50)
+    is_super_over = models.IntegerField()
+    wide_runs = models.IntegerField()
+    bye_runs = models.IntegerField()
+    legbye_runs = models.IntegerField()
+    noball_runs = models.IntegerField()
+    penalty_runs = models.IntegerField()
+    batsman_runs = models.IntegerField()
+    extra_runs = models.IntegerField()
+    total_runs = models.IntegerField(null=True, blank=True)
+    player_dismissed = models.CharField(max_length=50, null=True, blank=True)
+    dismissal_kind = models.CharField(max_length=50, null=True, blank=True)
+    fielder = models.CharField(max_length=50, null=True, blank=True)
