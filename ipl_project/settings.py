f9ae96f63156de95791fc9a2b8bb0bf9c85fe41f
Heera <heera1191@gmail.com>
2020-04-26 21:18:53 +0530

    chages

--- ipl_project/settings.py
+++ ipl_project/settings.py
@@ -159,5 +159,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
 
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
