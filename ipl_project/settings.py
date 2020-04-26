631af1f387c398c56d25710e616ad06a082310fc
Heera <heera1191@gmail.com>
2020-04-26 21:33:05 +0530

    static file changes

--- ipl_project/settings.py
+++ ipl_project/settings.py
@@ -145,6 +145,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
